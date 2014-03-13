eb48ed09c3621cd3ac94c1d9cdbe310cf0f44eaf
Pietro Gagliardi <pietro10@mac.com>
2014-03-12 21:42:25 -0400

    Added a note in ui.Go() about it needing to be run from main().

--- init.go
+++ init.go
@@ -7,6 +7,8 @@ package ui
 // After it returns, you cannot call future ui functions/methods meaningfully.
 // (TODO ideally we would want to be able to call ui.MsgBoxError() to report failures to the user, but I would need to figure out how to do this on platforms other than Windows.)
 //
+// It is not safe to call ui.Go() in a goroutine. It must be called directly from main().
+//
 // This model is undesirable, but Cocoa limitations require it.
 func Go(main func()) error {
 	return ui(main)
