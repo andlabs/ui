c766cb965f5bf3278582761fe3acd7a06a2de270
Pietro Gagliardi <pietro10@mac.com>
2014-03-19 21:16:35 -0400

    Added complementary GLib/GObject version compatibility macros to gtk_unix.h alongside the GDK/GTK+ ones.

--- gtk_unix.h
+++ gtk_unix.h
@@ -3,10 +3,12 @@
 /* This header file is a convenience to ensure that the compatibility flags below are included in all Go files that include <gtk/gtk.h> */
 
 /*
-MIN_REQUIRED signals that programs are expected to run on at least GTK+ 3 .4 and thus deprectation warnings for newer versions (such as gtk_scrolled_window_add_with_viewport() being deprecated in GTK+ 3.8) should be suppressed.
-MAX_ALLOWED signals that programs will not use features introduced in newer versions of GTK+ and that the compiler should warn us if we slip.
+MIN_REQUIRED signals that programs are expected to run on at least GLib 2.32/GTK+ 3.4 and thus deprectation warnings for newer versions (such as gtk_scrolled_window_add_with_viewport() being deprecated in GTK+ 3.8) should be suppressed.
+MAX_ALLOWED signals that programs will not use features introduced in newer versions of GLib/GTK+ and that the compiler should warn us if we slip.
 Thanks to desrt in irc.gimp.net/#gtk+
 */
+#define GLIB_VERSION_MIN_REQUIRED GLIB_VERSION_2_32
+#define GLIB_VERSION_MAX_ALLOWED GLIB_VERSION_2_32
 #define GDK_VERSION_MIN_REQUIRED GDK_VERSION_3_4
 #define GDK_VERSION_MAX_ALLOWED GDK_VERSION_3_4
 
