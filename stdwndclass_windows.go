d1a41cd95a7d3779e67619f99ae288a59da0eb1c
Pietro Gagliardi <pietro10@mac.com>
2014-03-24 11:36:15 -0400

    Corrected a copy-paste fail in stdwndclass_windows.go that broke compilation (this is why you test builds on all platforms, folks!) and added a TODO about resizing on Windows in the meantime.

--- stdwndclass_windows.go
+++ stdwndclass_windows.go
@@ -54,7 +54,8 @@ func stdWndProc(s *sysData) func(hwnd _HWND, uMsg uint32, wParam _WPARAM, lParam
 				}
 				// top-left corner is (0,0) so no need for winheight
 				s.resizes = s.resizes[0:0]		// set len to 0 without changing cap
-				s.resize(0, 0, width, height, &s.resizes)
+				s.resize(int(r.Left), int(r.Top), int(r.Right), int(r.Bottom), &s.resizes)
+				// TODO use the Defer movement functions here?
 				for _, s := range s.resizes {
 					err = s.sysData.setRect(s.x, s.y, s.width, s.height, 0)
 					if err != nil {
