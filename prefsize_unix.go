63cfe8d1fc7de969b328e802ae28520f8cd062f3
Pietro Gagliardi <pietro10@mac.com>
2014-04-27 16:26:06 -0400

    Removed height-for-width TODO from prefsize_unix.go after discussion with Company in irc.gimp.net/#gtk+.

--- prefsize_unix.go
+++ prefsize_unix.go
@@ -12,9 +12,8 @@ import (
 // ...actually, it tells us two things: the "minimum size" and the "natural size".
 // The "minimum size" is the smallest size we /can/ display /anything/. The "natural size" is the smallest size we would /prefer/ to display.
 // The difference? Minimum size takes into account things like truncation with ellipses: the minimum size of a label can allot just the ellipses!
-// So we use the natural size instead, right?
-// We could, but there's one snag: "Handle with care. Note that the natural height of a height-for-width widget will generally be a smaller size than the minimum height, since the required height for the natural width is generally smaller than the required height for the minimum width."
-// This will have to be taken care of manually, so TODO; we'll just use the natural size for now
+// So we use the natural size instead.
+// There is a warning about height-for-width controls, but in my tests this isn't an issue.
 
 func (s *sysData) preferredSize() (width int, height int) {
 	_, _, width, height = gtk_widget_get_preferred_size(s.widget)
