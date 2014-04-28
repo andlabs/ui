4452de9a07338cd9876bbb351b1b8f9ea5412b27
Pietro Gagliardi <pietro10@mac.com>
2014-04-28 02:15:19 -0400

    Removed a TODO from init.go that was already in the future plans document.

--- init.go
+++ init.go
@@ -6,7 +6,6 @@ package ui
 // If initialization fails, Go returns an error and main is not called.
 // Otherwise, Go does not return to its caller until main does, at which point it returns nil.
 // After it returns, you cannot call future ui functions/methods meaningfully.
-// (TODO ideally we would want to be able to call ui.MsgBoxError() to report failures to the user, but I would need to figure out how to do this on platforms other than Windows.)
 //
 // It is not safe to call ui.Go() in a goroutine. It must be called directly from main().
 //
