e2063cf94667b7317b90a54521e2f1345f2685d2
Pietro Gagliardi <pietro10@mac.com>
2014-05-25 16:40:15 -0400

    Integrated windowsconstgen into the test build script. There's a small issue regarding amd64...

--- test.sh
+++ test.sh
@@ -1,2 +1,16 @@
+if [ ! -f tools/windowsconstgen.go ]; then
+	echo error: $0 must be run from the package source root 1>&2
+	exit 1
+fi
+set -e
+if [ x$GOOS = xwindows ]; then
+	# have to build windowsconstgen as the host, otherwise weird things happen
+	wcg=`mktemp /tmp/windowsconstgenXXXXXXXXXXXX`
+	GOOS= GOARCH= go build -o $wcg tools/windowsconstgen.go
+	# but we can run it regardless of $GOOS/$GOARCH
+	$wcg . 386 "$@"
+	$wcg . amd64 "$@"
+	rm $wcg
+fi
 cd test
 go build "$@"
