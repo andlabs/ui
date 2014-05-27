9c86bf8d30b8f858d7d26ef3b8e1890a3cc96704
Pietro Gagliardi <pietro10@mac.com>
2014-05-26 22:36:34 -0400

    Merged windows_windows.go into common_windows.go.

--- common_windows.go
+++ common_windows.go
@@ -70,3 +70,26 @@ type _RECT struct {
 func negConst(c int) uintptr {
 	return uintptr(c)
 }
+
+var (
+	_adjustWindowRectEx = user32.NewProc("AdjustWindowRectEx")
+	_createWindowEx = user32.NewProc("CreateWindowExW")
+	_getClientRect = user32.NewProc("GetClientRect")
+	_moveWindow = user32.NewProc("MoveWindow")
+	_setWindowLong = user32.NewProc("SetWindowLongW")
+	_setWindowPos = user32.NewProc("SetWindowPos")
+	_setWindowText = user32.NewProc("SetWindowTextW")
+	_showWindow = user32.NewProc("ShowWindow")
+)
+
+type _MINMAXINFO struct {
+	ptReserved		_POINT
+	ptMaxSize		_POINT
+	ptMaxPosition		_POINT
+	ptMinTrackSize		_POINT
+	ptMaxTrackSize	_POINT
+}
+
+func (l _LPARAM) MINMAXINFO() *_MINMAXINFO {
+	return (*_MINMAXINFO)(unsafe.Pointer(l))
+}
