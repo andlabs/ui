15afca6402857d5a792af975f1e05c0d864461ae
Pietro Gagliardi <pietro10@mac.com>
2014-06-08 10:07:44 -0400

    Added the new MsgBox() behavior to the GTK+ backend. It /almost/ works right; just needs some more changes...

--- dialog_unix.go
+++ dialog_unix.go
@@ -22,59 +22,73 @@ import (
 // }
 import "C"
 
-func _msgBox(parent *Window, primarytext string, secondarytext string, msgtype C.GtkMessageType, buttons C.GtkButtonsType) (result C.gint) {
-	ret := make(chan C.gint)
-	defer close(ret)
-	uitask <- func() {
-		var pwin *C.GtkWindow = nil
-
-		// to implement parent, we need to put the GtkMessageDialog into a new window group along with parent
-		// a GtkWindow can only be part of one group
-		// so we use this to save the parent window group (if there is one) and store the new window group
-		// after showing the message box, we restore the previous window group, so future parent == nil can work properly
-		// thanks to pbor and mclasen in irc.gimp.net/#gtk+
-		var prevgroup *C.GtkWindowGroup = nil
-		var newgroup *C.GtkWindowGroup
-
-		if parent != nil {
-			pwin = togtkwindow(parent.sysData.widget)
-			// we can't remove a window from the "default window group"; otherwise this throws up Gtk-CRITICAL warnings
-			if C.gtk_window_has_group(pwin) != C.FALSE {
-				prevgroup = C.gtk_window_get_group(pwin)
-				C.gtk_window_group_remove_window(prevgroup, pwin)
+func _msgBox(parent *Window, primarytext string, secondarytext string, msgtype C.GtkMessageType, buttons C.GtkButtonsType) (result chan int) {
+	result = make(chan int)
+	go func() {
+		ret := make(chan C.gint)
+		defer close(ret)
+		uitask <- func() {
+			var pwin *C.GtkWindow = nil
+	
+			// to implement parent, we need to put the GtkMessageDialog into a new window group along with parent
+			// a GtkWindow can only be part of one group
+			// so we use this to save the parent window group (if there is one) and store the new window group
+			// after showing the message box, we restore the previous window group, so future parent == nil can work properly
+			// thanks to pbor and mclasen in irc.gimp.net/#gtk+
+			var prevgroup *C.GtkWindowGroup = nil
+			var newgroup *C.GtkWindowGroup
+	
+			if parent != nil {
+				pwin = togtkwindow(parent.sysData.widget)
+				// we can't remove a window from the "default window group"; otherwise this throws up Gtk-CRITICAL warnings
+				if C.gtk_window_has_group(pwin) != C.FALSE {
+					prevgroup = C.gtk_window_get_group(pwin)
+					C.gtk_window_group_remove_window(prevgroup, pwin)
+				}
+				newgroup = C.gtk_window_group_new()
+				C.gtk_window_group_add_window(newgroup, pwin)
 			}
-			newgroup = C.gtk_window_group_new()
-			C.gtk_window_group_add_window(newgroup, pwin)
+	
+			cprimarytext := C.CString(primarytext)
+			defer C.free(unsafe.Pointer(cprimarytext))
+			csecondarytext := (*C.char)(nil)
+			if secondarytext != "" {
+				csecondarytext = C.CString(secondarytext)
+				defer C.free(unsafe.Pointer(csecondarytext))
+			}
+			box := C.gtkNewMsgBox(pwin, msgtype, buttons, cprimarytext, csecondarytext)
+			response := C.gtk_dialog_run((*C.GtkDialog)(unsafe.Pointer(box)))
+			C.gtk_widget_destroy(box)
+	
+			if parent != nil {
+				C.gtk_window_group_remove_window(newgroup, pwin)
+				C.g_object_unref(C.gpointer(unsafe.Pointer(newgroup)))		// free the group
+				if prevgroup != nil {
+					C.gtk_window_group_add_window(prevgroup, pwin)
+				}		// otherwise it'll go back into the default group on its own
+			}
+	
+			ret <- response
 		}
-
-		cprimarytext := C.CString(primarytext)
-		defer C.free(unsafe.Pointer(cprimarytext))
-		csecondarytext := (*C.char)(nil)
-		if secondarytext != "" {
-			csecondarytext = C.CString(secondarytext)
-			defer C.free(unsafe.Pointer(csecondarytext))
-		}
-		box := C.gtkNewMsgBox(pwin, msgtype, buttons, cprimarytext, csecondarytext)
-		response := C.gtk_dialog_run((*C.GtkDialog)(unsafe.Pointer(box)))
-		C.gtk_widget_destroy(box)
-
-		if parent != nil {
-			C.gtk_window_group_remove_window(newgroup, pwin)
-			C.g_object_unref(C.gpointer(unsafe.Pointer(newgroup)))		// free the group
-			if prevgroup != nil {
-				C.gtk_window_group_add_window(prevgroup, pwin)
-			}		// otherwise it'll go back into the default group on its own
-		}
-
-		ret <- response
-	}
-	return <-ret
+		result <- int(<-ret)
+	}()
+	return result
 }
 
-func msgBox(parent *Window, primarytext string, secondarytext string) {
-	_msgBox(parent, primarytext, secondarytext, C.GtkMessageType(C.GTK_MESSAGE_OTHER), C.GtkButtonsType(C.GTK_BUTTONS_OK))
+func (w *Window) msgBox(primarytext string, secondarytext string) (done chan struct{}) {
+	done = make(chan struct{})
+	go func() {
+		<-_msgBox(w, primarytext, secondarytext, C.GtkMessageType(C.GTK_MESSAGE_OTHER), C.GtkButtonsType(C.GTK_BUTTONS_OK))
+		done <- struct{}{}
+	}()
+	return done
 }
 
-func msgBoxError(parent *Window, primarytext string, secondarytext string) {
-	_msgBox(parent, primarytext, secondarytext, C.GtkMessageType(C.GTK_MESSAGE_ERROR), C.GtkButtonsType(C.GTK_BUTTONS_OK))
+func (w *Window) msgBoxError(primarytext string, secondarytext string) (done chan struct{}) {
+	done = make(chan struct{})
+	go func() {
+		<-_msgBox(w, primarytext, secondarytext, C.GtkMessageType(C.GTK_MESSAGE_ERROR), C.GtkButtonsType(C.GTK_BUTTONS_OK))
+		done <- struct{}{}
+	}()
+	return done
 }
