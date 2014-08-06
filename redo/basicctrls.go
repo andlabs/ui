b8bf4f28b68ac5e571b0a63f1b3b4ec5b175c1ae
Pietro Gagliardi <pietro10@mac.com>
2014-08-05 20:48:42 -0400

    More TODOs.

--- redo/basicctrls.go
+++ redo/basicctrls.go
@@ -88,6 +88,7 @@ func NewTab() Tab {
 // Label shows one line of text; any text that does not fit is truncated.
 // A Label can either have smart vertical alignment relative to the control to its right or just be vertically aligned to the top (standalone).
 // The effect of placing a non-standalone Label in any context other than to the immediate left of a Control (TODO currently basic Control only? recheck Stack/Grid code) is undefined.
+// Both types of labels currently are left-aligned (TODO).
 type Label interface {
 	Control
 
