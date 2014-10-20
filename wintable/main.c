3385761294926e34f98d6a0b51026aa3139b2f77
Pietro Gagliardi <pietro10@mac.com>
2014-10-20 16:08:56 -0400

    More TODOs.

--- wintable/main.c
+++ wintable/main.c
@@ -24,6 +24,7 @@
 // TODO
 // - http://blogs.msdn.com/b/oldnewthing/archive/2003/09/09/54826.aspx (relies on the integrality parts? IDK)
 // 	- might want to http://blogs.msdn.com/b/oldnewthing/archive/2003/09/17/54944.aspx instead
+// - http://msdn.microsoft.com/en-us/library/windows/desktop/bb775574%28v=vs.85%29.aspx
 
 #define tableWindowClass L"gouitable"
 
