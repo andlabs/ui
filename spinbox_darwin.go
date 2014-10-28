b8cb342724997ad8f2a68881f073ee5559ef58d8
Pietro Gagliardi <pietro10@mac.com>
2014-10-28 18:25:05 -0400

    Wrote some notes on Mac OS X Spinbox.

---
+++ spinbox_darwin.go
@@ -0,0 +1,14 @@
+// 28 october 2014
+
+package ui
+
+// #include "objc_darwin.h"
+import "C"
+
+// interface builder notes
+// - the tops of the alignment rects should be identical
+// - spinner properties: auto repeat
+// - http://stackoverflow.com/questions/702829/integrate-nsstepper-with-nstextfield we'll need to bind the int value :S
+// 	- TODO experiment with a dummy project
+// TODO
+// - proper spacing between edit and spinner: Interface Builder isn't clear; NSDatePicker doesn't spill the beans
