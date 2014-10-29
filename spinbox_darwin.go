d73590e0201be32d3d468529c705d50842715db9
Pietro Gagliardi <pietro10@mac.com>
2014-10-28 20:29:37 -0400

    More Mac Spinbox stuff.

--- spinbox_darwin.go
+++ spinbox_darwin.go
@@ -10,5 +10,8 @@ import "C"
 // - spinner properties: auto repeat
 // - http://stackoverflow.com/questions/702829/integrate-nsstepper-with-nstextfield we'll need to bind the int value :S
 // 	- TODO experiment with a dummy project
+// - http://juliuspaintings.co.uk/cgi-bin/paint_css/animatedPaint/059-NSStepper-NSTextField.pl
+// - http://www.youtube.com/watch?v=ZZSHU-O7HVo
+// - http://andrehoffmann.wordpress.com/tag/nsstepper/ ?
 // TODO
 // - proper spacing between edit and spinner: Interface Builder isn't clear; NSDatePicker doesn't spill the beans
