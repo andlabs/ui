cd96f8ee2e0a4ae3370a57357f9bcde4e2a8c36f
Pietro Gagliardi <pietro10@mac.com>
2014-11-05 13:08:06 -0500

    Implemented TextField.ReadOnly() on GTK+.

--- textfield_unix.go
+++ textfield_unix.go
@@ -19,6 +19,7 @@ import "C"
 
 type textfield struct {
 	*controlSingleWidget
+	editable	*C.GtkEditable
 	entry   *C.GtkEntry
 	changed *event
 }
@@ -27,6 +28,7 @@ func startNewTextField() *textfield {
 	widget := C.gtk_entry_new()
 	t := &textfield{
 		controlSingleWidget: newControlSingleWidget(widget),
+		editable:	(*C.GtkEditable)(unsafe.Pointer(widget)),
 		entry:   (*C.GtkEntry)(unsafe.Pointer(widget)),
 		changed: newEvent(),
 	}
@@ -72,6 +74,16 @@ func (t *textfield) Invalid(reason string) {
 	defer freegstr(creason)
 	C.gtk_entry_set_icon_tooltip_text(t.entry, C.GTK_ENTRY_ICON_SECONDARY, creason)
 	C.gtk_widget_error_bell(t.widget)
+}
+
+// note that the property here is editable, which is the opposite of read-only
+
+func (t *textfield) ReadOnly() bool {
+	return !fromgbool(C.gtk_editable_get_editable(t.editable))
+}
+
+func (t *textfield) SetReadOnly(readonly bool) {
+	C.gtk_editable_set_editable(t.editable, togbool(!readonly))
 }
 
 //export textfieldChanged
