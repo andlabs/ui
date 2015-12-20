01893daaf19c3617b65820a7e1febdfa02beb389
Pietro Gagliardi <pietro10@mac.com>
2015-12-20 18:35:01 -0500

    I need MsgBoxError() for something, so threw it in.

---
+++ stddialogs.go
@@ -0,0 +1,16 @@
+// 20 december 2015
+package ui
+
+// #include "ui.h"
+import "C"
+
+// TODO OpenFile, SaveFile, MsgBox
+
+// TODO
+func MsgBoxError(w *Window, title string, description string) {
+	ctitle := C.CString(title)
+	cdescription := C.CString(description)
+	C.uiMsgBoxError(w.w, ctitle, cdescription)
+	freestr(ctitle)
+	freestr(cdescription)
+}
