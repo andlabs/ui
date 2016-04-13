42d273bea8068e47a1672f550e23f9cca2f1be27
Pietro Gagliardi <pietro10@mac.com>
2016-04-12 23:27:29 -0400

    More C.CBytes() preparatory work.

--- util.go
+++ util.go
@@ -13,8 +13,22 @@ import "C"
 
 // TODO move this to C.CBytes() when switching to Go 1.7
 
+// We want Go itself to complain when we're out of memory.
+// The allocators in cgo *should* do this, but there isn't a
+// C.CMalloc(). There *is* a C.CBytes(), however, for transferring
+// binary blobs from Go to C. If we pass this an arbitrary slice
+// of the desired length, we get our C.CMalloc(). Using a slice
+// that's always initialized to zero gives us the ZeroMemory()
+// for free.
+var uimallocBytes = make([]byte, 1024)		// 1024 bytes first
+
 //export uimalloc
 func uimalloc(n C.size_t) unsafe.Pointer {
+	if n > C.size_t(len(uimallocBytes)) {
+		// TODO round n up to a multiple of a power of 2?
+		// for instance 0x1234 bytes -> 0x1800 bytes
+		uimallocBytes = make([]byte, n)
+	}
 	p := C.malloc(n)
 	if p == nil {
 		panic("out of memory in uimalloc()")
