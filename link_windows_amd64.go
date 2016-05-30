80e7d83b77785aacc671f2327ccf135b6bcb0f09
Pietro Gagliardi <pietro10@mac.com>
2016-05-30 02:06:25 -0400

    This isn't going to work.

---
+++ link_windows_amd64.go
@@ -0,0 +1,12 @@
+// 13 december 2015
+
+package ui
+
+// #cgo LDFLAGS: ${SRCDIR}/libui_windows_amd64.lib ${SRCDIR}/libui_windows_amd64.res.o
+// /* note the order; also note the lack of uuid */
+// #cgo LDFLAGS: -luser32 -lkernel32 -lusp10 -lgdi32 -lcomctl32 -luxtheme -lmsimg32 -lcomdlg32 -ld2d1 -ldwrite -lole32 -loleaut32 -loleacc -lmsvcrt
+import "C"
+
+func ensureMainThread() {
+	// do nothing; Windows doesn't care which thread we're on so long as we don't change it after starting
+}
