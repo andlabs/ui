52f7d276a6bb04b8827ac019ad1e135b43819cea
Pietro Gagliardi <pietro10@mac.com>
2016-05-30 00:14:46 -0400

    Removed prev/.

--- prev/mergeback/table_mouseleave_windows.c
+++
@@ -1,4 +0,0 @@
-	case WM_MOUSELEAVE:
-		tablePushed(t->gotable, -1, -1);			// in case button held as drag out
-		// and let the list view do its thing
-		return (*fv_DefSubclassProc)(hwnd, uMsg, wParam, lParam);
