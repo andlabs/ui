b2f408abfc405da9b28912e5e790c9277b6e8a77
Vadzim Dambrouski <pftbest@gmail.com>
2016-06-10 01:34:16 +0300

    OpenFile, SaveFile and MsgBox dialogs

--- stddialogs.go
+++ stddialogs.go
@@ -5,13 +5,33 @@ package ui
 // #include "ui.h"
 import "C"
 
-// TODO OpenFile, SaveFile, MsgBox
-
 // TODO
 func MsgBoxError(w *Window, title string, description string) {
 	ctitle := C.CString(title)
 	cdescription := C.CString(description)
 	C.uiMsgBoxError(w.w, ctitle, cdescription)
+	freestr(ctitle)
+	freestr(cdescription)
+}
+
+func OpenFile(w *Window) string {
+	cname := C.uiOpenFile(w.w)
+	name := C.GoString(cname)
+	freestr(cname)
+	return name
+}
+
+func SaveFile(w *Window) string {
+	cname := C.uiSaveFile(w.w)
+	name := C.GoString(cname)
+	freestr(cname)
+	return name
+}
+
+func MsgBox(w *Window, title string, description string) {
+	ctitle := C.CString(title)
+	cdescription := C.CString(description)
+	C.uiMsgBox(w.w, ctitle, cdescription)
 	freestr(ctitle)
 	freestr(cdescription)
 }
