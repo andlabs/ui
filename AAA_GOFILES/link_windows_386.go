5b9b35a317d8b052f63ee155644104c350ff8b02
Pietro Gagliardi <pietro10@mac.com>
2018-09-02 03:22:45 -0400

    And moved the other link files back. It works!

--- AAA_GOFILES/link_windows_386.go
+++
@@ -1,8 +0,0 @@
-// 13 december 2015
-
-package ui
-
-// #cgo LDFLAGS: ${SRCDIR}/libui_windows_386.a
-// /* note the order; also note the lack of uuid */
-// #cgo LDFLAGS: -luser32 -lkernel32 -lusp10 -lgdi32 -lcomctl32 -luxtheme -lmsimg32 -lcomdlg32 -ld2d1 -ldwrite -lole32 -loleaut32 -loleacc -static -static-libgcc -static-libstdc++
-import "C"
---
+++ link_windows_amd64.go
@@ -0,0 +1,7 @@
+// 13 december 2015
+
+package ui
+
+// #cgo LDFLAGS: ${SRCDIR}/libui_windows_amd64.a
+// #cgo LDFLAGS: -luser32 -lkernel32 -lgdi32 -lcomctl32 -luxtheme -lmsimg32 -lcomdlg32 -ld2d1 -ldwrite -lole32 -loleaut32 -loleacc -luuid -lwindowscodecs -static -static-libgcc -static-libstdc++
+import "C"
