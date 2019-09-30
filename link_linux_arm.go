3d8ff6dcea24af2f3c561e7c72f81b502ce99be1
Jonathan Duck <duckbrain30@gmail.com>
2019-09-29 19:24:16 -0700

    Add ARM binary

---
+++ link_linux_arm.go
@@ -0,0 +1,12 @@
+// +build !windows
+// +build !darwin
+
+package ui
+
+// #cgo LDFLAGS: ${SRCDIR}/libui_linux_arm.a -lm -ldl
+// #cgo pkg-config: gtk+-3.0
+import "C"
+
+func ensureMainThread() {
+	// do nothing; GTK+ doesn't care which thread we're on so long as we don't change it after starting
+}
